added functionality for units, fixed axis ticks in animatins, and added eres plots to starry2

create_emaps.py
@@ -72,7 +72,7 @@ def set_emap_directory(fit):
 
 def emap_plot(star, indiv_path=None, proj='moll', other_fname=None, cmap = cm.bam, 
                 cmap_norm = None, transparent=False, colorbar=True, colorbar_label = True, colorbar_tick_rotation = 0,
-                fontsize=16, labels=True, title=None, border=True, ticks=True, gridlines=True, 
+                fontsize=16, labels=True, title=None, border=True, ticks=True, gridlines=True, units=None, 
                 unseen_line= False, cover_unseen=True, dpi = 300):
     
     """
@@ -137,6 +137,9 @@ def emap_plot(star, indiv_path=None, proj='moll', other_fname=None, cmap = cm.ba
     gridlines: boolean (optional)
         If True, will include grid lines.  Default is True
         Only works for proj="rect"
+
+    units: str (optional)
+        Units of flux, if None will say "[Normalized]" label.  Default is None
 
     unseen_line: boolean (optional)
         Whether to include line that separates visible from invisible regions according to star's inclination.
@@ -225,10 +228,16 @@ def emap_plot(star, indiv_path=None, proj='moll', other_fname=None, cmap = cm.ba
         plt.imshow(image, origin="lower", cmap=cmap, extent=extent)
 
     if colorbar:
+
         cbar = plt.colorbar(aspect = 40, pad = .03, shrink = 0.95)
         cbar.ax.tick_params(labelsize=fontsize * .60, rotation = colorbar_tick_rotation)
         if colorbar_label:
-            cbar.set_label("Flux [Normalized]", size=fontsize * .75, rotation = 270, labelpad = 15)
+            if units is None:
+                cbar.set_label("Flux [Normalized]", size=fontsize * .75, rotation = 270, labelpad = 15)
+            else:
+                cbar.set_label(f"Flux [{units}]", size=fontsize * .75, rotation = 270, labelpad = 15)
+
+        cbar.ax.yaxis.get_offset_text().set_visible(False)
 
     if labels:
         plt.xlabel("Longitude [deg]",fontsize=fontsize)
@@ -464,7 +473,7 @@ def create_emaps(star, eigeny, emaps_path=None, other_fname=None,
                 proj='moll', cmap = cm.bam, individual=True,
                 standard_cbar = True, center_flux=0, standard_indiv_cbar = True,
                 transparent=False, labels=True, title = None, border=True, 
-                ticks=False, gridlines=True, unseen_line=False, cover_unseen = True,
+                ticks=False, gridlines=True, units=None, unseen_line=False, cover_unseen = True,
                 fontsize=16, colorbar=True, colorbar_label = True, colorbar_tick_rotation = 0, dpi = 300):
     
     """
@@ -534,6 +543,9 @@ def create_emaps(star, eigeny, emaps_path=None, other_fname=None,
 
     gridlines: boolean (optional)
         If True, will add grid lines to each axis plot.  Default is True
+
+    units: str (optional)
+        Units of flux, if None will say "[Normalized]" in label.  Default is None
 
     unseen_line: boolean (optional)
         Whether to include line that separates visible from invisible regions according to star's inclination.
@@ -919,12 +931,18 @@ def create_emaps(star, eigeny, emaps_path=None, other_fname=None,
         fig.suptitle(title,fontsize=int(fontsize*1.5))
 
     fig.tight_layout()
-
     if colorbar and standard_cbar:
+
+
         cbar = fig.colorbar(im, ax=axes.ravel().tolist(), shrink=0.95, aspect = 40, pad = .03)
         cbar.ax.tick_params(labelsize=fontsize * .60, rotation = colorbar_tick_rotation)
         if colorbar_label:
-            cbar.set_label("Flux [Normalized]", size=fontsize * .75, rotation = 270, labelpad = 15)
+            if units is None:
+                cbar.set_label("Flux [Normalized]", size=fontsize * .75, rotation = 270, labelpad = 15)
+            else:
+                cbar.set_label(f"Flux [{units}]", size=fontsize * .75, rotation = 270, labelpad = 15)
+        
+        cbar.ax.yaxis.get_offset_text().set_visible(False)
 
     se(f"\n\tGenerating overall emap plot", dp = dpm)
     
@@ -947,7 +965,7 @@ def create_emaps(star, eigeny, emaps_path=None, other_fname=None,
 
 def create_eflux(star, eigeny, emaps_path=None, other_fname=None,
                  theta = np.linspace(-180, 180, 361),
-                 transparent=False, fontsize=16, 
+                 transparent=False, fontsize=16, units=None,
                  labels=True, title = None, border=True, ticks=False,
                  individual=True, color="sandybrown", 
                  centerline=True, cline_color="k", dpi=300):
@@ -984,6 +1002,9 @@ def create_eflux(star, eigeny, emaps_path=None, other_fname=None,
 
     fontsize: int (optional)
         Sets size of axis labels and title (1.5x axis).  Default is 16
+
+    units: str (optional)
+        Units of flux, if None will say "[Normalized]" label.  Default is None
 
     labels: boolean (optional)
         Wether to add axis labels to overall plot (a represents all).  Default to False
@@ -1113,6 +1134,8 @@ def create_eflux(star, eigeny, emaps_path=None, other_fname=None,
                 ax.set_yticklabels([f"{val:.2f}" for val in y_tick_values])
                 ax.set_ylim(min_f - amp*buffer, max_f + amp*buffer)
 
+        ax.yaxis.get_offset_text().set_visible(False)
+
         
         if not border:
             ax.set_frame_on(False)
@@ -1125,7 +1148,11 @@ def create_eflux(star, eigeny, emaps_path=None, other_fname=None,
 
     if labels:
         fig.supxlabel("Angle of rotation [degrees]",fontsize=fontsize)
-        fig.supylabel("Flux [normalized]",fontsize=fontsize)
+
+        if units is None:
+            fig.supylabel("Flux [normalized]",fontsize=fontsize)
+        else:
+            fig.supylabel(f"Flux [{units}]",fontsize=fontsize)
 
     if title:
         fig.suptitle(title,fontsize=int(fontsize*1.5))

create_real_null.py
@@ -281,7 +281,7 @@ def scale_real_map(realistic_star, uni_star_limb, scale_value, uni_comp, ratio_n
 
     
 def create_real_null(null_eigens, fit, results_path, theta=np.linspace(0,360,180), uni_comp = 1,
-                     scale_star=1, scaler="lc", cmap=cm.buda, store_scaled=True):
+                     scale_star=1, scaler="lc", cmap=cm.buda, store_scaled=True, units=None):
 
     se("\tCalculating Flux Range Limits\n", dp=dpm)
 
@@ -340,20 +340,20 @@ def create_real_null(null_eigens, fit, results_path, theta=np.linspace(0,360,180
         se(f'\t\t\u2022 Creating plots for "Null map {i}"', dp=dpm)
         
         create_rv.map_animations(realistic_star, theta = theta,fname=f"{results_path}/{folder_name}/emap_animation.mp4", 
-                       interval = ani_interval, transparent=False, cmap=cmap)
+                       interval = ani_interval, transparent=False, cmap=cmap, units=units)
         
         create_rv.map_animations(realistic_star, theta = theta[::2],fname=f"{results_path}/{folder_name}/emap_animation.gif", 
-                       interval = ani_interval, transparent=False, cmap=cmap)
+                       interval = ani_interval, transparent=False, cmap=cmap, units=units)
 
-        create_rv.flux_rv_line(realistic_star,
+        create_rv.flux_rv_line(realistic_star, units=units,
                      flux_name=f"{results_path}/{folder_name}/flux_curve.png",rv_name=f"{results_path}/{folder_name}/rv_curve.png")
         
         realistic_star.map.amp /= ratio_no_limb
         create_emaps.emap_plot(realistic_star, indiv_path=f"{results_path}/{folder_name}", proj='rect', other_fname=None, 
-                 transparent=False, colorbar=True, cmap=cmap)
+                 transparent=False, colorbar=True, cmap=cmap, units=units)
         
         create_emaps.emap_plot(realistic_star, indiv_path=f"{results_path}/{folder_name}", proj='moll', other_fname=None, 
-                 transparent=False, colorbar=True, cmap=cmap)
+                 transparent=False, colorbar=True, cmap=cmap, units=units)
         realistic_star.map.amp *= ratio_no_limb
 
         se(f'\033[38;5;208m\t\t"Null map {i}" plots are complete!\033[0m', dp=dpm)

starry2/maps.py
@@ -653,6 +653,83 @@ class MapBase(object):
             return tuple(images
                      + borders
                      + rv_image)
+        
+    def _updatefig_growing_long(
+        self,
+        i,
+        img_list,
+        image_list,
+        img_overlay,
+        overlay,
+        projection,
+        grid,
+        lonlines_list,
+        latlines_list,
+        borders,
+        kwargs,
+        extra_lines,
+        legend_list):
+        
+        img_list[0].set_array(image_list[0][i])
+        images = [img_list[0]]
+
+        img_list[1].set_array(image_list[1][i])
+        images2 = [img_list[1]]
+
+        if overlay is not None:
+            img_overlay.set_array(overlay[i])
+            images += [img_overlay]
+        if (
+            projection == STARRY_ORTHOGRAPHIC_PROJECTION
+            and grid
+            and len(image_list[0]) > 1
+            and self.nw is None
+        ):
+            lonlines = lonlines_list[0]
+            lonlines2 = lonlines_list[1]
+            lons = self._get_ortho_longitude_lines(i=i, **kwargs)
+            for n, l in enumerate(lons):
+                lonlines[n].set_xdata(l[0])
+                lonlines[n].set_ydata(l[1])
+
+                lonlines2[n].set_xdata(l[0])
+                lonlines2[n].set_ydata(l[1])
+
+        rv_image = extra_lines[0][1]
+        rv_data = extra_lines[0][0]
+
+
+        new_rv_data = np.concatenate((rv_data[:i], np.full(rv_data[i:].shape[0], np.nan)))
+        
+        # np.concatenate((rv_data[i:],rv_data[:i]))
+
+        rv_image[0].set_ydata(new_rv_data)
+
+        middle = rv_data.shape[0]//2
+
+        if legend_list and grid:
+            legend_list[1].get_texts()[0].set_text(f"$RV_0$: {round(new_rv_data[middle], 2):.2g}")
+            
+
+            return tuple(images + lonlines + images2 + lonlines2
+                     + latlines_list[0] + borders + latlines_list[1]
+                     + rv_image + legend_list)
+        
+        elif legend_list:
+            legend_list[1].get_texts()[0].set_text(f"$RV_0$: {round(new_rv_data[middle], 2):.2g}")
+
+            return tuple(images + images2
+                     + borders
+                     + rv_image + legend_list)
+        elif grid:
+            return tuple(images + lonlines  + images2 + lonlines2 
+                     + latlines_list[0] + borders + latlines_list[1]
+                     + rv_image)
+        
+        else:
+            return tuple(images + images2
+                     + borders
+                     + rv_image)
 
     def reset(self, **kwargs):
         """Reset all map coefficients and attributes.
@@ -765,6 +842,7 @@ class MapBase(object):
         bitrate = kwargs.pop("bitrate", None)
         colorbar = kwargs.pop("colorbar", False)
         colorbar_label = kwargs.pop("colorbar_label", None)
+        cbar_format = kwargs.pop("cbar_format", None)
         #added label for colorbar
         colorbar_size = kwargs.pop("colorbar_size", "5%")
         colorbar_pad = kwargs.pop("colorbar_pad", 0.03)
@@ -1000,10 +1078,14 @@ class MapBase(object):
                 position="bottom", size=colorbar_size, pad=colorbar_pad
             )
             if colorbar_label:
-                cbar = fig.colorbar(img, cax=cax, orientation="horizontal")
+                cbar = fig.colorbar(img, cax=cax, orientation="horizontal",format=cbar_format)
                 cbar.set_label(colorbar_label, size=colorbar_fontsize)
             else:
-                fig.colorbar(img, cax=cax, orientation="horizontal")
+                fig.colorbar(img, cax=cax, orientation="horizontal",format=cbar_format)
+
+            cbar.ax.xaxis.get_offset_text().set_visible(False)
+
+        
 
         # Add a colorbar
         elif colorbar:
@@ -1014,10 +1096,10 @@ class MapBase(object):
                 position="right", size=colorbar_size, pad=colorbar_pad, 
             )
             if colorbar_label:
-                cbar = fig.colorbar(img, cax=cax, orientation="vertical")
+                cbar = fig.colorbar(img, cax=cax, orientation="vertical",format=cbar_format)
                 cbar.set_label(colorbar_label, size=colorbar_fontsize)
             else:
-                fig.colorbar(img, cax=cax, orientation="vertical")
+                fig.colorbar(img, cax=cax, orientation="vertical",format=cbar_format)
 
         # Display or save the image / animation
         if animated:
@@ -1056,35 +1138,66 @@ class MapBase(object):
 
             elif extra_image and extra_lines:
 
-                img_list = [img,extra_image[0]]
-                image_list = [image,extra_image[1]]
-                lonlines_list = [lonlines,extra_image[2]]
-                latlines_list = [latlines,extra_image[3]]
+                if is_growing:
+                    img_list = [img,extra_image[0]]
+                    image_list = [image,extra_image[1]]
+                    lonlines_list = [lonlines,extra_image[2]]
+                    latlines_list = [latlines,extra_image[3]]
 
-                ani = FuncAnimation(
-                    fig,
-                    self._updatefig_all,
-                    fargs=(
-                        img_list,
-                        image_list,
-                        img_overlay,
-                        overlay,
-                        projection,
-                        grid,
-                        lonlines_list,
-                        latlines_list,
-                        borders,
-                        kwargs,
-                        extra_lines,
-                        legend_list,
-                    ),
-                    interval=interval,
-                    blit=blit_status,
-                    frames=image.shape[0],
-                )
+                    ani = FuncAnimation(
+                        fig,
+                        self._updatefig_growing_long,
+                        fargs=(
+                            img_list,
+                            image_list,
+                            img_overlay,
+                            overlay,
+                            projection,
+                            grid,
+                            lonlines_list,
+                            latlines_list,
+                            borders,
+                            kwargs,
+                            extra_lines,
+                            legend_list,
+                        ),
+                        interval=interval,
+                        blit=blit_status,
+                        frames=image.shape[0],
+                    )
+
+                else:
+
+                    img_list = [img,extra_image[0]]
+                    image_list = [image,extra_image[1]]
+                    lonlines_list = [lonlines,extra_image[2]]
+                    latlines_list = [latlines,extra_image[3]]
+
+                    ani = FuncAnimation(
+                        fig,
+                        self._updatefig_all,
+                        fargs=(
+                            img_list,
+                            image_list,
+                            img_overlay,
+                            overlay,
+                            projection,
+                            grid,
+                            lonlines_list,
+                            latlines_list,
+                            borders,
+                            kwargs,
+                            extra_lines,
+                            legend_list,
+                        ),
+                        interval=interval,
+                        blit=blit_status,
+                        frames=image.shape[0],
+                    )
                 
 
             elif extra_image:
+
 
                 img_list = [img,extra_image[0]]
                 image_list = [image,extra_image[1]]
@@ -2365,6 +2478,9 @@ class LimbDarkenedBase(object):
         return ()
     
     def _updatefig_growing(i):
+        return ()
+
+    def _updatefig_growing_long(i):
         return ()
 
     def _get_flux_kwargs(self, kwargs):

unspotable.py
@@ -70,7 +70,18 @@ def unspotable(cfile):
     se("\n\033[32mCreating realistic null maps:\033[0m", dp = dpm)
     se("----------------------------------------------------------------------------", dp = dpm)
 
-    create_real_null.create_real_null(null_eigens, fit, results_path)
+    if fit.cfg.star.units:
+        scaler = 5.670374419e-8 * fit.cfg.star.teff**4
+        power = int(np.floor(np.log10(scaler)))
+        if power > 6:
+            units = f"W m$^{{-2}}$ * 10$^{power}$"
+        else:
+            units = f"W m$^{{-2}}$"
+    else:
+        scaler = 1
+        units = None
+
+    create_real_null.create_real_null(null_eigens, fit, results_path, units=units, scale_star=scaler)
 
 if __name__ == "__main__":
     # Uncomment if you want to see command line arguments 